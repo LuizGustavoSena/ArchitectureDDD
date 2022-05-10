2209fae4b87a8dbe20fc14568c7e61e529bc62f2
Luiz Gustavo Banin de Sena <luiz.sena@voeazul.com.br>
2022-05-10 20:36:53 -0300

    feature: Class CRUD entity

---
+++ src/Api.Data/Repository/BaseRepository.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Api.Data.Context;
+using Api.Domain.Entities;
+using Api.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Repository
+{
+    public class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly MyContext _context;
+        private readonly DbSet<T> _dataset;
+        public BaseRepository(MyContext context)
+        {
+            _context = context;
+            _dataset = _context.Set<T>();
+        }
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            try
+            {
+                var result = await SelectAsync(id);
+
+                if (result == null)
+                    return false;
+
+                _dataset.Remove(result);
+
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return true;
+        }
+
+        public async Task<T> InsertAsync(T item)
+        {
+            try
+            {
+                if (item.Id == Guid.Empty)
+                    item.Id = Guid.NewGuid();
+
+                item.CreateAt = DateTime.UtcNow;
+
+                _dataset.Add(item);
+
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return item;
+        }
+
+        public async Task<T> SelectAsync(Guid id)
+        {
+            try
+            {
+                return await _dataset.SingleOrDefaultAsync(e => e.Id.Equals(id));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<IEnumerable<T>> SelectAsync()
+        {
+            try
+            {
+                return await _dataset.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<T> UpdateAsync(T item)
+        {
+            try
+            {
+                var result = await SelectAsync(item.Id);
+
+                if (result == null)
+                    return null;
+
+                item.CreateAt = result.CreateAt;
+                item.UpdateAt = DateTime.UtcNow;
+
+                _context.Entry(result).CurrentValues.SetValues(item);
+
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return item;
+        }
+    }
+}
--- src/Api.Domain/Interfaces/IRepository.cs
+++ src/Api.Domain/Interfaces/IRepository.cs
@@ -7,7 +7,8 @@ namespace Api.Domain.Interfaces
 {
     public interface IRepository<T> where T : BaseEntity
     {
-        Task<T> SelectAsync(T item);
+        Task<T> InsertAsync(T item);
+        Task<T> SelectAsync(Guid id);
         Task<T> UpdateAsync(T item);
         Task<bool> DeleteAsync(Guid id);
         Task<IEnumerable<T>> SelectAsync();
