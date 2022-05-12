bb3a29befb5abd137ef30619691cc727fed83f6d
Luiz Gustavo Banin de Sena <luiz.sena@voeazul.com.br>
2022-05-12 19:40:14 -0300

    fix: LoginService Class

--- src/Api.Service/Service/LoginService.cs
+++ src/Api.Service/Service/LoginService.cs
@@ -1,17 +1,37 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using Api.Domain.Dtos;
+using Api.Domain.Entities;
 using Api.Domain.Interfaces.Service.User;
 using Api.Domain.Repository;
+using Api.Domain.Security;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api.Service.Service
 {
     public class LoginService : ILoginService
     {
         private IUserRepository _repositoy;
+        private SigningConfigurations _signingConfigurations;
+        private TokenConfigurations _tokenConfigurations;
+        private IConfiguration _configuration;
 
-        public LoginService(IUserRepository repository)
+        public LoginService(IUserRepository repository,
+                            SigningConfigurations signingConfigurations,
+                            TokenConfigurations tokenConfigurations,
+                            IConfiguration configuration)
         {
             _repositoy = repository;
+
+            _signingConfigurations = signingConfigurations;
+
+            _tokenConfigurations = tokenConfigurations;
+
+            _configuration = configuration;
         }
 
         public async Task<object> FindByLogin(LoginDto user)
@@ -19,7 +39,62 @@ namespace Api.Service.Service
             if (user == null || string.IsNullOrWhiteSpace(user.Email))
                 return null;
 
-            return await _repositoy.FindByEmal(user.Email);
+            var baseUser = await _repositoy.FindByEmal(user.Email);
+
+            if (baseUser == null)
+                return new
+                {
+                    Authenticated = false,
+                    Message = "Falha na autenticação"
+                };
+
+            var identity = new ClaimsIdentity(
+                new GenericIdentity(baseUser.Email),
+                new[]{
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.UniqueName, baseUser.Email)
+                }
+            );
+
+            var createDate = DateTime.Now;
+            var expirationDate = createDate + TimeSpan.FromSeconds(_tokenConfigurations.Seconds);
+
+            var handler = new JwtSecurityTokenHandler();
+            var token = CreateToken(identity, createDate, expirationDate, handler);
+
+            return SuccessObject(createDate, expirationDate, token, baseUser);
+        }
+
+        private string CreateToken(ClaimsIdentity identity, DateTime createDate, DateTime expirationDate, JwtSecurityTokenHandler handler)
+        {
+            var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _tokenConfigurations.Issuer,
+                Audience = _tokenConfigurations.Audience,
+                SigningCredentials = _signingConfigurations.SigningCredentials,
+                Subject = identity,
+                NotBefore = createDate,
+                Expires = expirationDate
+            });
+
+            return handler.WriteToken(securityToken);
+        }
+
+        private object SuccessObject(DateTime createDate, DateTime expirationDate, string token, UserEntity user)
+        {
+            return new
+            {
+                authenticated = true,
+                created = createDate.ToString("YYYY/MM/dd HH:mm:ss"),
+                expiration = expirationDate.ToString("YYYY/MM/dd HH:mm:ss"),
+                accessToken = token,
+                user = new
+                {
+                    name = user.Name,
+                    Email = user.Email
+                },
+                message = "Usuário logado",
+            };
         }
     }
 }
