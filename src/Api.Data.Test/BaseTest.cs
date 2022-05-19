39bd55d2a6ff0a1103a5c6866beae4a4efc25fb9
Luiz Gustavo Banin de Sena <luiz.sena@voeazul.com.br>
2022-05-19 20:34:16 -0300

    feature: BaseTest

--- src/Api.Data.Test/BaseTest.cs
+++ src/Api.Data.Test/BaseTest.cs
@@ -1,6 +1,6 @@
 using System;
 using Microsoft.Extensions.DependencyInjection;
-
+using Api.Data.Context;
 namespace Api.Data.Test
 {
     public abstract class BaseTest
@@ -13,12 +13,31 @@ namespace Api.Data.Test
 
     public class DbTest : IDisposable
     {
-        public string baseNameDatabase = $"dbApi_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+        public string dataBaseName = $"dbApi_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
         public ServiceProvider ServiceProvider { get; private set; }
+
+        public DbTest()
+        {
+            var serviceCollection = new ServiceCollection();
+            var connectionString = $"Persist Security Info-True;Server=localhost;Database={dataBaseName};User=root;Password=mudar@123";
+            serviceCollection.AddDbContext<MyContext>(
+                options => options.UseSqlServer(connectionString),
+                serviceLifeTime.Transient
+            );
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureDeleted();
+            }
         }
     }
 }
